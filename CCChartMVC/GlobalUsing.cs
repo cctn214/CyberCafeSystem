99b3ee28dbb1efaf6bdf7b317d7fbbc92969ccdb
cctn214 <soebalacoding214@gmail.com>
2026-09-10 21:06:05 +0700

    replace json serializer with newtonsoft json and add global using

---
+++ CCChartMVC/GlobalUsing.cs
@@ -0,0 +1,6 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using Newtonsoft.Json;
+global using CCDatabase.Models;
+global using CCChartMVC.Models;
